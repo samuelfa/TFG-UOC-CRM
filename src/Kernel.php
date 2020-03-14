a86c4defb5acc298c30b78c641b562f5216e5388
Samuel Fernández Amorós <samuel@sumacrm.com>
2020-03-14 14:38:01 +0100

    Ignore error

--- src/Kernel.php
+++ src/Kernel.php
@@ -17,7 +17,8 @@ class Kernel extends BaseKernel
 
     public function registerBundles(): iterable
     {
-        $contents = require $this->getProjectDir().'/config/bundles.php';
+        /** @noinspection PhpIncludeInspection */
+        $contents = require $this->getProjectDir() . '/config/bundles.php';
         foreach ($contents as $class => $envs) {
             if ($envs[$this->environment] ?? $envs['all'] ?? false) {
                 yield new $class();
