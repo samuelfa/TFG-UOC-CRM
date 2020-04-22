fa784a333dc45eb6535098274178e91fddb760f5
Samuel Fernández Amorós <samuel@sumacrm.com>
2020-04-23 00:22:48 +0200

    Register events from crm

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
 import $ from 'jquery';
 import 'bootstrap';
 import registerHandler from './register';
-import './sb-admin'
+import adminHandler from './sb-admin'
 
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
     registerHandler().registerEvents();
+    adminHandler().registerEvents();
 });
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
