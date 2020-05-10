d325cb0321dfc318599bda64a6e4f59ee0dc6fde
Samuel Fernández Amorós <samuel@sumacrm.com>
2020-05-10 18:42:16 +0200

    Fix error after register a new account

--- src/Infrastructure/Symfony/Security/AuthenticateAfterRegister.php
+++ src/Infrastructure/Symfony/Security/AuthenticateAfterRegister.php
@@ -4,6 +4,8 @@
 namespace App\Infrastructure\Symfony\Security;
 
 
+use App\Domain\Employee\Employee;
+use App\Domain\Employee\ManagerRepository;
 use App\Domain\Employee\WorkerRepository;
 use App\Domain\ValueObject\NIF;
 use App\Infrastructure\Symfony\Factory\URLFactory;
@@ -14,31 +16,46 @@ class AuthenticateAfterRegister
 {
     private GuardAuthenticatorHandler $authenticatorHandler;
     private Authenticator $authenticator;
-    private WorkerRepository $repository;
+    private WorkerRepository $workerRepository;
+    private ManagerRepository $managerRepository;
     private URLFactory $URLFactory;
 
     public function __construct(
         GuardAuthenticatorHandler $authenticatorHandler,
         Authenticator $authenticator,
-        WorkerRepository $repository,
+        WorkerRepository $workerRepository,
+        ManagerRepository $managerRepository,
         URLFactory $URLFactory
     )
     {
         $this->authenticatorHandler = $authenticatorHandler;
         $this->authenticator = $authenticator;
-        $this->repository = $repository;
+        $this->workerRepository = $workerRepository;
+        $this->managerRepository = $managerRepository;
         $this->URLFactory = $URLFactory;
     }
 
     public function authenticate(NIF $nif, string $namespace, Request $request): string
     {
-        $user = $this->repository->findOneByNif($nif);
-        if(!$user){
-            throw new \RuntimeException('User not found');
-        }
+        $user = $this->findOneByNif($nif);
 
         $this->authenticatorHandler->authenticateUserAndHandleSuccess($user, $request, $this->authenticator, 'main');
         return $this->URLFactory->generate($namespace);
+    }
+
+    private function findOneByNif(NIF $nif): Employee
+    {
+        $user = $this->workerRepository->findOneByNif($nif);
+        if($user){
+            return $user;
+        }
+
+        $user = $this->managerRepository->findOneByNif($nif);
+        if($user){
+            return $user;
+        }
+
+        throw new \RuntimeException('User not found');
     }
 
 }
