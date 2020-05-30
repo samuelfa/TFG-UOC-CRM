97585b7d703ef9f1be706b33d571a3855b9982f9
Samuel Fernández Amorós <samuel@sumacrm.com>
2020-05-30 17:33:26 +0200

    Add support to detect the browser language as default language if the language is able

--- src/Infrastructure/Symfony/Event/LocaleSubscriber.php
+++ src/Infrastructure/Symfony/Event/LocaleSubscriber.php
@@ -10,26 +10,23 @@ use Symfony\Component\HttpKernel\KernelEvents;
 
 class LocaleSubscriber implements EventSubscriberInterface
 {
-    private string $defaultLocale;
+    /** @var string[] */
+    private array  $availableLanguages;
 
-    public function __construct($defaultLocale = 'en')
+    public function __construct(array $availableLanguages)
     {
-        $this->defaultLocale = $defaultLocale;
+        $this->availableLanguages = $availableLanguages;
     }
 
     public function onKernelRequest(RequestEvent $event): void
     {
         $request = $event->getRequest();
-        if (!$request->hasPreviousSession()) {
-            return;
-        }
 
-        // try to see if the locale has been set as a _locale routing parameter
         if ($locale = $request->attributes->get('_locale')) {
             $request->getSession()->set('_locale', $locale);
         } else {
-            // if no explicit locale has been set on this request, use one from the session
-            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+            $defaultLocale = $request->getPreferredLanguage($this->availableLanguages);
+            $request->setLocale($request->getSession()->get('_locale', $defaultLocale));
         }
     }
 
